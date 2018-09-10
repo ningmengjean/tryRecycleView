f01c9e8a9fb68ab70afbe1d31ff42bfec2f6a968
ningmengjean <ningmengjean@gmail.com>
2018-09-10 11:17:44 +0800

    first commit

---
+++ tryRecycleView/MyCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MyCollectionViewCell.swift
+//  tryRecycleView
+//
+//  Created by addictedtoelixir on 2017/10/7.
+//  Copyright © 2017年 wangchi. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ tryRecycleView/MyFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  MyFlowLayout.swift
+//  tryRecycleView
+//
+//  Created by addictedtoelixir on 2017/10/7.
+//  Copyright © 2017年 wangchi. All rights reserved.
+//
+
+import UIKit
+
+class MyFlowLayout: UICollectionViewFlowLayout {
+
+}
